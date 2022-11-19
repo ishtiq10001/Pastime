acf01baebc26078f0f8148b744cbc193d6861e2f
Syxd Iltxfxt <40826265+ishtiq10001@users.noreply.github.com>
2022-11-18 21:41:39 -0500

    Update loginTest.py

--- login system/loginTest.py
+++ login system/loginTest.py
@@ -3,7 +3,7 @@ import math
 from time import time,sleep
 import hashlib
 
-
+u_p_dict = {}
 def read_file():
     global u_p_dict
     uFile_U = open("usersC.txt", "r")
@@ -15,44 +15,80 @@ def read_file():
 
     u_1 = users.split(',')
     p_1 = passw.split(',')
-    u_p_dict = {}
+    
 
     for u,p in zip(u_1,p_1):
         u_p_dict[u] = p
 
-    print(f"{u_p_dict}")
-    #start
+
+    
+
+def check_user(userN):
+    if userN == "":
+        return False
+    if userN in u_p_dict:
+        
+        return False
+    else:
+        
+        return True
 
 
-
-
-
-def check_user(userN,passW):
-    pass
-
-
-def signUp():
+def signup():
     user_N = input("enter username: ")
-    pass_w = getpass("enter password: ")
-    conf_p = getpass("confirm password: ")
+    if check_user(user_N) == False:
+        print("username already exists")
+        return signup()    
+    else:
+        pass
+    
+    pass_w = input("enter password: ")
+    conf_p = input("confirm password: ")
+    
+    if (check_user(user_N) == True) and (pass_w != conf_p):
+        print("password is not the same as above")
+        return signup()
 
-    if pass_w == conf_p:
+    elif (check_user(user_N) == True) and (pass_w == conf_p):
+        if len(pass_w) < 8 or ',' in pass_w:
+            print("password either too short or there is an invalid character there")
+            return signup()
+        else:
+            pass
         enc = conf_p.encode()
         hash0 = hashlib.md5(enc).hexdigest()
-        print(hash0)
 
-        with open("usersC.txt", "w") as uFile_U:
-            uFile_U.write(user_N+"\n")
+        with open("usersC.txt", "a") as uFile_U:
+            uFile_U.write(user_N+",")
             uFile_U.close()
-        with open("passwordsC.txt", "w") as uFile_P:
-            uFile_P.write(hash0+"\n")
+        with open("passwordsC.txt", "a") as uFile_P:
+            uFile_P.write(hash0+",")
             uFile_P.close()
 
         print("registration successful")
+        return
+    else:
+        print("unexpected error")
+        return signup()
+
+
+def login():
+    userName = input("enter username: ")
+    passWord = input("enter password: ")
+    read_file()
+    if userName not in u_p_dict():
+        return login()
 
     else:
-        print("password is not the same as above")
+        for i,j in u_p_dict.items():
+            if userName == i and passWord == j:
+                return True
+            else:
+                continue
+                
 
 
 if __name__ == '__main__':#main "method"
     read_file()
+    signup()
+    
